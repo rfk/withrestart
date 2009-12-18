3011eb0a73ceea76cc146a38427cbed057773e6a
Ryan Kelly <ryan@rfk.id.au>
2009-12-18 16:26:01 +1100

    bump version number

--- withrestart/__init__.py
+++ withrestart/__init__.py
@@ -161,7 +161,7 @@ there's no shame in trying to pinch a good idea when you see one...
 
 __ver_major__ = 0
 __ver_minor__ = 2
-__ver_patch__ = 0
+__ver_patch__ = 1
 __ver_sub__ = ""
 __version__ = "%d.%d.%d%s" % (__ver_major__,__ver_minor__,
                               __ver_patch__,__ver_sub__)
